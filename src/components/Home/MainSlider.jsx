f500eafcdd8b403a6791bb8da1f2cf14cdb66213
unknown <osamadrbas31@gmail.com>
2023-09-25 12:26:54 +0300

    New

--- src/components/Home/MainSlider.jsx
+++ src/components/Home/MainSlider.jsx
@@ -7,30 +7,67 @@ import 'swiper/css/navigation'
 import { useSwiper } from 'swiper/react'
 import { EffectFade, Navigation, Pagination } from 'swiper/modules'
 function MainSlider() {
-    const imgs = [
+    const imgsl = [
         'https://n.nordstrommedia.com/it/eebd2371-223e-440e-9964-20fb7fc6c04f.jpeg?h=700&w=1608',
         'https://n.nordstrommedia.com/it/e5d46602-8d9b-4406-89e0-e2150299c03e.jpeg?h=700&w=1608',
         'https://n.nordstrommedia.com/it/30b9fb57-05b7-45f2-a7a0-824c102a7c9e.jpeg?h=700&w=1608'
     ]
-  return (
-    <Swiper
-    effect={'fade'}
-    modules={[EffectFade]}
-    loop={true}
-    className="mySwiper"
-  >
-    {imgs.map((i,index)=>{
-        return(
-        <SwiperSlide key={index}>
-            <SwiperCard image={i}/>
-        </SwiperSlide>
-        )
-    })}
-  </Swiper>
+    const imgss = [
+      'https://n.nordstrommedia.com/it/aa4c4aeb-bf1b-432d-8077-38a14c46a614.jpeg?h=750&w=750',
+      'https://n.nordstrommedia.com/it/f90ef97f-811b-4ed0-bbc8-03190c428d38.jpeg?h=750&w=750',
+      'https://n.nordstrommedia.com/it/79500d38-d305-4599-93a6-bc16d6355b93.jpeg?h=750&w=750'
+    ]
+  return (<>
+    <Lg imgs={imgsl}></Lg>
+    <Sm imgs={imgss}></Sm>
+    </>
   )
 }
 
-const SwiperCard = ({image})=>{
+const Lg = ({imgs})=>{
+  return(
+    <div className='hidden phone:block'>
+        <Swiper
+        effect={'fade'}
+        modules={[EffectFade]}
+        loop={true}
+        className="mySwiper"
+      >
+        {imgs.map((i,index)=>{
+            return(
+            <SwiperSlide key={index}>
+                <SwiperCardL image={i}/>
+            </SwiperSlide>
+            )
+        })}
+      </Swiper>
+  </div>
+  )
+}
+
+const Sm = ({imgs})=>{
+  return(
+    <div className='phone:hidden'>
+        <Swiper
+        effect={'fade'}
+        modules={[EffectFade]}
+        loop={true}
+        className="mySwiper"
+      >
+        {imgs.map((i,index)=>{
+            return(
+            <SwiperSlide key={index}>
+                <SwiperCards image={i}/>
+            </SwiperSlide>
+            )
+        })}
+      </Swiper>
+  </div>
+  )
+}
+
+
+const SwiperCardL = ({image})=>{
     const swiper = useSwiper()
     return(
     <>
@@ -45,9 +82,33 @@ const SwiperCard = ({image})=>{
 
                  </NavLink>
             </div>
-             <button ></button>
 
     </>)
+}
+
+
+const SwiperCards = ({image})=>{
+  const swiper = useSwiper()
+  return(
+  <> 
+      <div className='flex flex-col'>
+          <div className='pb-[100%] imgBack relative flex group' style={{backgroundImage: `url(${image})`}}>
+               <button onClick={() => swiper.slideNext()} className='  absolute top-[45%] left-[2%] z-10 text-[150%] hover:bg-dark2 hover:text-white flex items-center justify-center transition-all duration-200 h-[10%]'><ion-icon name="chevron-back-outline"></ion-icon></button>
+               <button onClick={() => swiper.slideNext()} className='  absolute top-[45%] left-[92%] z-10 text-[150%] hover:bg-dark2 hover:text-white flex items-center justify-center transition-all duration-200 h-[10%]'><ion-icon name="chevron-forward-outline"></ion-icon></button>
+
+               <NavLink to="/store" className="w-full h-full  absolute">
+
+               </NavLink>
+          </div>
+          <div className='flex flex-col px-[5%]'>
+                    <label className='text-[140%] ' htmlFor="">Fall Escape</label>
+                    <p htmlFor="">Get lost in a lush new season with picture-perfect outerwear, cozy knits and more</p>
+                 
+          </div>
+      </div>
+
+
+  </>)
 }
 
 export default MainSlider
--- src/components/Home/SimDiv.jsx
+++ src/components/Home/SimDiv.jsx
@@ -3,13 +3,13 @@ import React from 'react'
 function SimDiv() {
   return (
     <div className='flex flex-row   mt-[2%] mb-[5%]'>
-    <div style={{backgroundImage:'url(https://tpc.googlesyndication.com/simgad/15721634154017957185)'}} className='w-[50%] imgBack flex flex-row'>
+    <div style={{backgroundImage:'url(https://tpc.googlesyndication.com/simgad/15721634154017957185)'}} className=' w-[60%] phone:w-[50%] imgBack flex flex-row'>
 
     </div>
-    <div className='w-[50%] text-center py-[2.7%] bg-dark2 text-white flex flex-col items-center justify-center '>
-          <label className=' font-semibold phone:text-[110%]' htmlFor="">  English Pear & Sweet Pea </label>
+    <div className='  w-[40%] px-[.7%] phone:px-[0%] phone:w-[50%] text-center phoen:py-[2.7%] py-[3%] bg-dark2 text-white flex flex-col items-center justify-center '>
+          <label className=' font-semibold text-[90%] phone:text-[110%]' htmlFor="">  English Pear & Sweet Pea </label>
           <label className='hidden ms:block' htmlFor=""> A Brand-New Cologne from Jo Malone London</label>
-          <label className='border-b-[1px] box border-white mt-[1%]' htmlFor="">Shop Now</label>
+          <label className='border-b-[1px] box border-white text-[85%] mt-[1%]' htmlFor="">Shop Now</label>
 
     </div>
 </div>
