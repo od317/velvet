c708fbb3068e4cf1215cc47ef6af8b5d3965fe73
unknown <osamadrbas31@gmail.com>
2023-10-04 08:50:13 +0300

    new

--- src/components/Product/Bottom.jsx
+++ src/components/Product/Bottom.jsx
@@ -8,8 +8,6 @@ import { useSwiperSlide } from 'swiper/react'
 import ProductsSlider from './ProductsSlider'
 
 function Bottom({product}) {
-
-
   return (
     <div className='flex flex-col  mb-[5%]'>
         <label className=' font-bold text-[140%] px-[4%] mt-[5%] mb-[0%] text-dark2'>
@@ -19,6 +17,6 @@ function Bottom({product}) {
         <ProductsSlider products={product.simList}/>
         </div>
     </div>
-  );
+  )
 }
 export default Bottom
