273fd8bc3f0a809863836c7bafc486e422a84c19
unknown <osamadrbas31@gmail.com>
2023-10-08 16:00:56 +0300

    new

--- src/components/store/Sort.jsx
+++ src/components/store/Sort.jsx
@@ -53,21 +53,21 @@ const LargeSort = ({handleSortChange,sort})=>{
                       <label className='cursor-pointer  ' htmlFor="">Sort by: {sort}</label>
                       <label className={`${showSort ? ' rotate-180 ':''} flex items-center justify-center text-center transition-all duration-200`} htmlFor=""><ion-icon name="chevron-down-outline"></ion-icon></label>
                     </div>
-                    <div className={`flex ${showSort ? 'max-h-screen  phone:border-black':'max-h-[0rem]  phone:border-p1 '}  transition-all duration-300 tmaxh bg-p1 flex-col text-start border-t-0 border-[1px]  w-full absolute`}>
+                    <div className={`flex ${showSort ? 'max-h-screen  phone:border-black border-t-0':'max-h-[0rem]  phone:border-p1 '} box-content transition-all duration-300 tmaxh bg-p1 flex-col text-start border-t-0 border-[1px]  w-full absolute`}>
                           <div className='h-full overflow-hidden'>
-                              <button onClick={()=>handleClick('featured')} className='w-full hover:bg-light2 transition-all duration-100 cursor-pointer text-start px-[5%]'>featured</button>
+                              <button onClick={()=>handleClick('featured')} className='w-full  my-[1%] hover:bg-light2 transition-all duration-100 cursor-pointer text-start px-[2%]'>featured</button>
                           </div>
                           <div className='h-full overflow-hidden'>
-                              <button onClick={()=>handleClick('price low to high')} className='w-full hover:bg-light2 transition-all duration-100 cursor-pointer  text-start px-[5%]'>price low to high</button>
+                              <button onClick={()=>handleClick('price low to high')} className='w-full  my-[1%] hover:bg-light2 transition-all duration-100 cursor-pointer  text-start px-[2%]'>price low to high</button>
                           </div>
                           <div className='h-full overflow-hidden'>
-                              <button onClick={()=>handleClick('price high to low')} className='w-full hover:bg-light2 transition-all duration-100 cursor-pointer text-start px-[5%]'>price high to low</button>
+                              <button onClick={()=>handleClick('price high to low')} className='w-full  my-[1%] hover:bg-light2 transition-all duration-100 cursor-pointer text-start px-[2%]'>price high to low</button>
                           </div>
                           <div className='h-full overflow-hidden'>
-                             <button onClick={()=>handleClick('rate')} className='w-full hover:bg-light2 transition-all duration-100 cursor-pointer text-start px-[5%]'>rating</button>
+                             <button onClick={()=>handleClick('rate')} className='w-full  my-[1%] hover:bg-light2 transition-all duration-100 cursor-pointer text-start px-[2%]'>rating</button>
                           </div>
                           <div className='h-full overflow-hidden'>
-                            <button onClick={()=>handleClick('date')} className='w-full hover:bg-light2 transition-all duration-100 cursor-pointer text-start px-[5%]'>date</button>
+                            <button onClick={()=>handleClick('date')} className='w-full  my-[1%] hover:bg-light2 transition-all duration-100 cursor-pointer text-start px-[2%]'>date</button>
                           </div>
                     </div>
                </div>
