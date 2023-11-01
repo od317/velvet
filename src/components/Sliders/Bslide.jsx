897477176acd20c686dbd6ffe5bc77197ee838c4
unknown <osamadrbas31@gmail.com>
2023-11-01 16:16:57 +0200

    New

--- src/components/Sliders/Bslide.jsx
+++ src/components/Sliders/Bslide.jsx
@@ -3,8 +3,8 @@ import Small from './Small'
 import Large from './Large'
 function Bslide({items,num,text}) {
   return (<>
-    <Small items={items} text={text}></Small>
-    <Large num={num} text={text} items={items}></Large>
+    {/* <Small items={items} text={text}></Small>
+    <Large num={num} text={text} items={items}></Large> */}
      </>
   )
 }
